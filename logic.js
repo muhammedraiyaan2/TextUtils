4cad9e6670e9041581ed596741ee775e38a850c5
Muhammad Raiyaan <83336994+muhammedraiyaan2@users.noreply.github.com>
2021-08-18 10:59:28 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,13 @@
+let texta=document.getElementById('textareas')
+let text2=document.getElementById('text2')
+let submit=document.getElementById('submit')
+let clear=document.getElementById('clear')
+let textup=document.getElementById("textup")
+let text=texta.value
+clear.addEventListener("click",function(){
+    texta.value=""
+})
+submit.addEventListener("click",function(){
+    text2.innerText=`${texta.value.length} letter, ${texta.value.split(" ").length} words`
+    document.write(`${toUpperCase(texta.value)}`)
+})
